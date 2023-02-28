85a24df9839750c7f4ad261a7d78efcaca59f504
dongckim <dck.alx@gmail.com>
2023-02-28 23:17:40 +0900

    2023.02.28 updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import HeaderInput from './component/HeaderInput'
 import Main from './component/Main'
@@ -8,16 +8,31 @@ import Footer from './component/Footer'
 function App() {
   return (
     <div>
-      <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" style={{width : '300px', height : '100px'}}/>
-    <div className='app-template'>
-      <HeaderInput/>
-      <Main/>
-      <p className='border-bottom'></p>
-      <Footer/>
+      <img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" style={{width : '200px', height : '70px'}}/>
+      <div className='app-template'>
+        <HeaderInput EntireFunction = {EntireFunction()}/>
+        <Main/>
+        <p className='border-bottom'></p>
+        <Footer/>
+      </div>
     </div>
-    </div>
-    
   )
 }
+
+const EntireFunction = (props) => {
+  const [todo, setTodo] = useState([
+    { id: 1, content: '리팩토링 많이 해보기', isDone: 'false' },
+    { id: 2, content: '리액트 빨리 끝내기', isDone: 'false' },
+    { id: 3, content: '웹 개발 완벽하게 마무리하고 시작!', isDone: 'false' }
+  ])
+
+  const [content, setContent] = useState('');
+  
+  const dataObj = {todo, content};
+  const funcObj = {setTodo, setContent};
+  return {dataObj, funcObj}
+}
+
+
 
 export default App;
--- src/component/HeaderInput.jsx
+++ src/component/HeaderInput.jsx
@@ -1,15 +1,22 @@
 import { FaPlus } from "react-icons/fa";
+import { clickAddButtonHandler } from "../function";
 
-const HeaderInput = () => {
+const HeaderInput = ({dataObj, funcObj}) => {
+    console.log(dataObj, funcObj)
+    // const [ setTodo, setContent ] = funcObj; 
+    // console.log({dataObj, funcObj})
     return(
         <form className="todo__input">
             <input 
                 className="input"
                 type="text"
-                // onChange={this.handleChange}
                 placeholder="Add a task here..."
+                // // onChange={(event) =>{
+                // //         setContent(event.target.value)
+                // //     } 
+                // }
             />
-            <button type="submit" className="button-1">
+            <button type="submit" className="button-1" onChange={(event) => {clickAddButtonHandler(event, dataObj, funcObj)}}>
                 <FaPlus/>
             </button>
         </form>
