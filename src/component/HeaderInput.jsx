ea9a53693539f55959d2482a3481adc23deb3de2
dongckim <dck.alx@gmail.com>
2023-03-01 17:30:33 +0900

    Final To-do List Done.

--- src/component/HeaderInput.jsx
+++ src/component/HeaderInput.jsx
@@ -1,10 +1,7 @@
 import InsertButton from "./InsertButton";
 import InputContents from "./InputContents";
-import { useContext } from "react";
-import { TodoContext } from "../App";
 
 const HeaderInput = () => {
-    const { todo, setTodo, content,setContent } = useContext(TodoContext);
     return(
         <>
             <div className="todo__input" >
